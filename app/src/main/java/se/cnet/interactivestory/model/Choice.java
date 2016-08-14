922c5a5c8d622b16701fc86e0165d6d944bec0c9
Vivian Esquivias <v.esquivias@gmail.com>
2016-08-14 17:18:53 +0200

    Initial commit

---
+++ app/src/main/java/se/cnet/interactivestory/model/Choice.java
@@ -0,0 +1,29 @@
+package se.cnet.interactivestory.model;
+
+/**
+ * Created by Vivi on 2016-08-12.
+ */
+public class Choice {
+    private String mText;
+    private int mNextPage;
+
+    public Choice(String text, int nextPage){
+        mText = text;
+        mNextPage = nextPage;
+    }
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public int getNextPage() {
+        return mNextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        mNextPage = nextPage;
+    }
+}
